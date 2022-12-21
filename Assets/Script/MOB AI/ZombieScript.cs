6e0440aa7b428787e2a157fad69f4c7484bf37b0
KeaneNKY <88319315+KeaneNKY@users.noreply.github.com>
2022-12-22 03:58:03 +0800

    healthbars
    added health bar for mob and player

---
+++ Assets/Script/MOB AI/ZombieScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZombieScript : MonoBehaviour
+
+{
+    public Slider zhealthbar;
+
+
+    void Update()
+    {
+        //zhealthbar.value = HP;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    }
+//}
